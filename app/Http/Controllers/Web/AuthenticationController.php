ce1c67f0c3820f9ca7d0ec67fa4107dfca7a7115
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-03-12 21:22:23 +0800

    GitHub 登录认证

---
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthenticationController extends Controller
+{
+    public function getSocialRedirect($account)
+    {
+        try {
+            return Socialite::with($account)->redirect();
+        } catch (\InvalidArgumentException $e) {
+            return redirect('/login');
+        }
+    }
+
+    public function getSocialCallback($account)
+    {
+        $socialUser = Socialite::with($account)->user();
+
+        $user = User::where([
+            'provider' => $account,
+            'provider_id' => $socialUser->id
+        ])->first();
+
+        if ($user == null) {
+            $newUser = new User();
+
+            $newUser->name = $socialUser->getName();
+            $newUser->email = $socialUser->getEmail() ?? '';
+            $newUser->avatar = $socialUser->getAvatar();
+            $newUser->password = '';
+            $newUser->provider = $account;
+            $newUser->provider_id = $socialUser->getId();
+            $newUser->save();
+
+            $user = $newUser;
+        }
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}

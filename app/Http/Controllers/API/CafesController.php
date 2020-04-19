fd9acc3999c63aca96b5a57687a83a04cc9b16a2
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-04-19 14:51:08 +0800

    新增 Cafe model 和 完善 CafesController

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -3,22 +3,38 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cafe;
 use Illuminate\Http\Request;
 
 class CafesController extends Controller
 {
     public function getCafes()
     {
+        $cafes = Cafe::all();
 
+        return response()->json($cafes);
     }
 
     public function getCafe($id)
     {
+        $cafe = Cafe::where('id', $id)->first();
+
+        return response()->json($cafe);
 
     }
 
-    public function postNewCafe()
+    public function postNewCafe(Request $request)
     {
+        $cafe = new Cafe();
 
+        $cafe->name = $request->input('name');
+        $cafe->address = $request->input('address');
+        $cafe->city = $request->input('city');
+        $cafe->state = $request->input('state');
+        $cafe->zip = $request->input('zip');
+
+        $cafe->save();
+
+        return response()->json($cafe, 201);
     }
 }

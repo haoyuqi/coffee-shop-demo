e3996b389b0753261257e1389aa0452876572656
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-04-19 14:32:35 +0800

    新增 api route 和 CafesController

---
+++ app/Http/Controllers/API/CafesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CafesController extends Controller
+{
+    public function getCafes()
+    {
+
+    }
+
+    public function getCafe($id)
+    {
+
+    }
+
+    public function postNewCafe()
+    {
+
+    }
+}

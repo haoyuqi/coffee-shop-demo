e9ff9da17f32439c388d0455806a8dbdfd371b70
Hao Yuqi <hi.haoyuqi@gmail.com>
2020-05-18 21:19:52 +0800

    新增 vuex 模块

---
+++ resources/js/modules/cafes.js
@@ -0,0 +1,76 @@
+import CafesAPI from '../api/cafe.js';
+
+export const cafes = {
+    state: {
+        cafes: [],
+        casesLoadStatus: 0,
+        cafe: {},
+        cafeLoadStatus: 0
+    },
+
+    actions: {
+        loadCafes({commit}) {
+            commit('setCafesLoadStatus', 1);
+
+            CafesAPI.getCafes()
+                .then(function (response) {
+                    commit('setCafes', response.data);
+                    commit('setCafesLoadStatus', 2);
+                })
+                .catch(function (response) {
+                    commit('setCafes', []);
+                    commit('setCafesLoadStatus', 3);
+                });
+        },
+
+        loadCafe({commit}, data) {
+            commit('setCafeLoadStatus', 1);
+
+            CafesAPI.getCafe(data.id)
+                .then(function (response) {
+                    commit('setCafe', response.data);
+                    commit('setCafeLoadStatus', 2);
+                })
+                .catch(function (response) {
+                    commit('setCafes', []);
+                    commit('setCafeLoadStatus', 3);
+                });
+        }
+    },
+
+    mutations: {
+        setCafesLoadStatus(state, status) {
+            state.cafesLoadStatus = status;
+        },
+
+        setCafes(state, cafes) {
+            state.cafes = cafes;
+        },
+
+        setCafeLoadStatus(state, status) {
+            state.cafeLoadStatus = status;
+        },
+
+        setCafe(state, cafe) {
+            state.cafe = cafe;
+        }
+    },
+
+    getters: {
+        getCafesLoadStatus(state) {
+            return state.cafesLoadStatus;
+        },
+
+        getCafes(state) {
+            return state.cafes;
+        },
+
+        getCafeLoadStatus(state) {
+            return state.cafeLoadStatus;
+        },
+
+        getCafe(state) {
+            return state.cafe;
+        }
+    }
+}
--- resources/js/store.js
+++ resources/js/store.js
@@ -4,7 +4,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+import { cafes } from './modules/cafes.js';
 
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        cafes
+    }
 })

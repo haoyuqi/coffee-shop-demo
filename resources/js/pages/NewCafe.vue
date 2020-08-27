bdaf07a2fd43fa9568d1473239ba703d2fc7b2a0
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-08-27 16:20:44 +0800

    标签功能

--- resources/js/pages/NewCafe.vue
+++ resources/js/pages/NewCafe.vue
@@ -78,6 +78,9 @@
                     </span>
                     </div>
                     <div class="large-12 medium-12 small-12 cell">
+                        <tags-input v-bind:unique="key"></tags-input>
+                    </div>
+                    <div class="large-12 medium-12 small-12 cell">
                         <a class="button" v-on:click="removeLocation(key)">移除位置</a>
                     </div>
                 </div>
@@ -95,6 +98,9 @@
 </template>
 
 <script>
+import TagsInput from "../components/global/forms/TagsInput.vue";
+import {EventBus} from "../event-bus.js";
+
 export default {
     data() {
         return {
@@ -144,7 +150,7 @@ export default {
         },
 
         addLocation: function () {
-            this.locations.push({name: '', address: '', city: '', state: '', zip: '', methodsAvailable: []})
+            this.locations.push({name: '', address: '', city: '', state: '', zip: '', methodsAvailable: [], tags: ''})
             this.validations.locations.push({
                 address: {
                     is_valid: true,
@@ -251,6 +257,7 @@ export default {
                 }
             };
 
+            EventBus.$emit('clear-tags');
             this.addLocation();
         }
     },
@@ -271,6 +278,16 @@ export default {
                 $("#cafe-added-unsuccessfully").show().delay(5000).fadeOut();
             }
         }
+    },
+
+    components: {
+        TagsInput
+    },
+
+    mounted() {
+        EventBus.$on('tags-edited', function (tagsAdded) {
+            this.locations[tagsAdded.unique].tags = tagsAdded.tags;
+        }.bind(this))
     }
 }
 </script>

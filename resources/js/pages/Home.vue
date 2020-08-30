4bfc824e4a12c76e07e33c0bd7cb5757fb9a4580
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-08-30 13:34:07 +0800

    前端过滤功能

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,20 @@
-<style>
+<style lang="scss">
+@import '~@/abstracts/_variables.scss';
 
+div#home {
+    a.add-cafe-button {
+        float: right;
+        display: block;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        background-color: $dark-color;
+        color: white;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+}
 </style>
 
 <template>
@@ -8,23 +23,37 @@
         <span v-show="cafesLoadStatus == 2">Cafes loaded successfully!</span>
         <span v-show="cafesLoadStatus == 3">Cafes loaded unsuccessfully!</span>
     </div>
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="large-12 medium-12 small-12 columns">
+                <router-link :to="{ name: 'newcafe' }" class="add-cafe-button">+ 新增咖啡店</router-link>
+            </div>
+        </div>
+    </div>
+
+    <cafe-filter></cafe-filter>
 </template>
 
 <script>
-    export default {
-        created() {
-            this.$store.dispatch('loadCafes')
-        },
+import CafeFilter from "../components/cafes/CafeFilter.vue";
 
-        computed: {
-            // 获取 cafes 加载状态
-            cafesLoadStatus() {
-                return this.$store.getters.getCafesLoadStatus
-            },
-            // 获取 cafes
-            cafes() {
-                return this.$store.getters.getCafes
-            }
+export default {
+    created() {
+        this.$store.dispatch('loadCafes')
+    },
+
+    computed: {
+        // 获取 cafes 加载状态
+        cafesLoadStatus() {
+            return this.$store.getters.getCafesLoadStatus
+        },
+        // 获取 cafes
+        cafes() {
+            return this.$store.getters.getCafes
         }
+    },
+    components: {
+        CafeFilter
     }
+}
 </script>

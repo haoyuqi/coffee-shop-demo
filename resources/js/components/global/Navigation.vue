2d0fd6124a07e976cd29681341e1875779ae13e7
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2021-07-04 17:05:36 +0800

    用户信息个人编辑

--- resources/js/components/global/Navigation.vue
+++ resources/js/components/global/Navigation.vue
@@ -75,6 +75,9 @@
 
         <div class="right">
             <img class="avatar" v-if="user !== '' && userLoadStatus === 2" :src="user.avatar" v-show="userLoadStatus === 2">
+            <router-link :to="{ name: 'profile'}" v-if="user !== '' && userLoadStatus === 2" class="profile">
+                个人信息
+            </router-link>
             <span class="logout" v-if="user !== '' && userLoadStatus === 2" @click="logout()">退出</span>
             <span class="login" v-if="user === ''" @click="login()">登录</span>
         </div>
